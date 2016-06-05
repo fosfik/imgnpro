5561d0684c3a731da2c4d0748412ee45a55fc832
jerh56@gmail.com <jerh56@gmail.com>
2016-06-05 09:36:15 -0600

    Se cambio la configuracion de la URL de nuevo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 	facebook:{
 		key:'1709249819339885',
-		secret:'4894456da8d734e0aa2685d202e9d489'
+		secret:'4894456da8d734e0aa2685d202e9d489',
 		callbackURL: 'http://imgnpro.herokuapp.com/login/facebook/return'
 	}
 

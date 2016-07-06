584324225acc308e6e1317d1471299f6867ead30
IMGNPro <imgn.profesional@gmail.com>
2016-07-06 13:24:47 -0600

    Se cambio la ruta de la  BD productiva
    Se cambio la ruta de la  BD productiva porque estaba mal

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'url_test' : 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r',
-   'url' : 'mongodb://localhost/passport'
+  'url' : 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r',
+   'url_test' : 'mongodb://localhost/passport'
 }

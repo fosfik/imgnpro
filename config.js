8ad093998c4db9caa5ba51345f5ab28c0b65dccd
Johnny <jerh56@gmail.com>
2016-09-11 09:23:13 -0600

    Se modificó el archivo de configuración para tener 3 ambientes, Desarrollo, pruebas y producción.

--- config.js
+++ config.js
@@ -5,20 +5,11 @@ var config;
 // se puede configurar npm el proxy
 // npm config set proxy http://proxy.company:8080
 
-if (app.get('env') !== 'development') {
+if (app.get('env') !== 'development' || app.get('env') !== 'test') {
  
  config = {
 	// Produccion
-	// facebook:{
-	// 	key:'1748473698730930',
-	// 	secret:'947279e657965fc061e89ff28bc0d4fe',
-	// 	callbackURL: 'https://www.imgnpro.com/login/facebook/return'
-	// }
-	// facebook:{
-	// 	key:'1569270823378058',
-	// 	secret:'fb521481fdae215007b136a12905ff94',
-	// 	callbackURL: 'https://www.imgnpro.com/login/facebook/return'
-	// }
+
 
 	facebook:{
 		appname:'IMGN Pro',
@@ -67,46 +58,88 @@ if (app.get('env') !== 'development') {
 	};
 }
 else{
-  config = {
-	// Produccion
-	// facebook:{
-	// 	key:'1748473698730930',
-	// 	secret:'947279e657965fc061e89ff28bc0d4fe',
-	// 	callbackURL: 'http://localhost:3000/login/facebook/return'
-	// }
-	
-	// // Prueba
-	facebook:{
-		appname:'Photomaker',
-		key:'1709249819339885',
-		secret:'4894456da8d734e0aa2685d202e9d489',
-		callbackURL: 'http://localhost:3000/login/facebook/return'
-	},
-	google:{
-		key:'812259967962-gcvtg69gr1grfhbhi09poagqmjb1gkrl.apps.googleusercontent.com',
-		secret:'7naFjeqHyO1ZG6xEV8Vw9EYI',
-		callbackURL: 'http://localhost:3000/login/google/return'
-	},
-	prices:{
 
-		// hasta el el dia de hoy:
+	if (app.get("env")!=='test'){
+		config = {
+		// Desarrollo
+		
+		facebook:{
+			appname:'Photomaker',
+			key:'1709249819339885',
+			secret:'4894456da8d734e0aa2685d202e9d489',
+			callbackURL: 'http://localhost:3000/login/facebook/return'
+		},
+		google:{
+			key:'812259967962-gcvtg69gr1grfhbhi09poagqmjb1gkrl.apps.googleusercontent.com',
+			secret:'7naFjeqHyO1ZG6xEV8Vw9EYI',
+			callbackURL: 'http://localhost:3000/login/google/return'
+		},
+		prices:{
 
-		// $1.50 Recorte / Remover Fondo a Imagen
+			// hasta el el dia de hoy:
 
-		// -Extras-
+			// $1.50 Recorte / Remover Fondo a Imagen
 
-		// $0.55 Sombra Natural
-		// $0.20 Drop Shadow
-		// $0.40 Correction Color
-		// $2.40 Clipping Path
-		// $0.60 Retoque Básico
-		cutandremove:'1.50',
-		naturalshadow:'0.55',
-		dropshadow:'0.20',
-		correctcolor:'0.40',
-		clippingpath:'2.40',
-		basicretouch:'0.60'
+			// -Extras-
+
+			// $0.55 Sombra Natural
+			// $0.20 Drop Shadow
+			// $0.40 Correction Color
+			// $2.40 Clipping Path
+			// $0.60 Retoque Básico
+			cutandremove:'1.50',
+			naturalshadow:'0.55',
+			dropshadow:'0.20',
+			correctcolor:'0.40',
+			clippingpath:'2.40',
+			basicretouch:'0.60'
+		}
+	  };
+
 	}
+	else
+	{
+
+	config = {
+		
+		//Prueba
+		facebook:{
+			appname:'IMGP Pro test',
+			key:'1095931290497105',
+			secret:'8d6cd07ca8b1fb2dbadb1750f5ec8bf1',
+			callbackURL: 'http://imgnprotest.herokuapp.com/login/facebook/return'
+		},
+		google:{
+			key:'812259967962-gcvtg69gr1grfhbhi09poagqmjb1gkrl.apps.googleusercontent.com',
+			secret:'7naFjeqHyO1ZG6xEV8Vw9EYI',
+			callbackURL: 'http://imgnprotest.herokuapp.com/login/google/return'
+		},
+		prices:{
+
+			// hasta el el dia de hoy:
+
+			// $1.50 Recorte / Remover Fondo a Imagen
+
+			// -Extras-
+
+			// $0.55 Sombra Natural
+			// $0.20 Drop Shadow
+			// $0.40 Correction Color
+			// $2.40 Clipping Path
+			// $0.60 Retoque Básico
+			cutandremove:'1.50',
+			naturalshadow:'0.55',
+			dropshadow:'0.20',
+			correctcolor:'0.40',
+			clippingpath:'2.40',
+			basicretouch:'0.60'
+		}
+	  };
+
+	}
+
+
+  
 	//,
 
 
@@ -132,7 +165,7 @@ else{
  //        api_key: '225134126558414', 
  //        api_secret: 'Fewu645wVZ_dGSN76IAYXgDt7m8' 
  //    }
-	};
+	
 }
 
 

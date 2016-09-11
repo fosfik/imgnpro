afcd58202e1e9a447315f8cf49bc9fe732b01b0b
Johnny <jerh56@gmail.com>
2016-09-11 09:51:12 -0600

    Se cambió la con para que la ruta de la BD la obtenga de la variable MONGODB_URI

--- models/db.js
+++ models/db.js
@@ -1,15 +1,23 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
-if (app.get('env') !== 'development') {
- var  url = 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r';
-}
-else{
-  var  url = 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r';	
- //var  url = 'mongodb://localhost/passport';
-}
+
+
+var url = process.env.MONGODB_URI;
+// if (app.get('env') === 'production') {
+//  var  url = 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r';
+// }
+// else{
+// 	if (app.get('env') === 'development'){
+// 		 var  url = 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r';	
+// 	}
+// 	else{
+//  		var  url = 'mongodb://admin:123456@ds051903.mlab.com:51903/heroku_554zpg9r';	
+// 	}
+// }
 mongoose.connect(url, function(err) {
     if (err){
+    	console.log('Hay un error al conectar:' + err);
 		throw err;
     } 
     else{

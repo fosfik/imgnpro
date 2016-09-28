a2e036462a5e3f175e08c0d2b8d4f5dc89c08aa3
IMGNPro <imgn.profesional@gmail.com>
2016-09-28 16:48:28 -0600

    Update index.js
    Se corrigió el error de lista de pedidos.

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ router.get('/listorders/:limit', function(req, res) {
       console.log('No se encontraron pedidos');
     }
    
-  }).select('imagecount numorder status date').sort('-date').limit(req.params.limit);
+  }).select('imagecount numorder status date').sort('-date').limit(parseInt(req.params.limit));
 });
 
 // TODO agregar seguridad a esta ruta

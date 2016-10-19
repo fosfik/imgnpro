1f953564aade41215f0766142c5b78051d0f1f2b
Johnny <jerh56@gmail.com>
2016-10-19 06:07:19 -0600

    Se modificó el tipo de cambio.

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ if ( app.get('env') === 'production') {
 		correctcolor:'0.40',
 		clippingpath:'2.40',
 		basicretouch:'0.60',
-		dollar:'19.38'
+		dollar:'19.04'
 	},
 		register:{
 			usermustactivate: false,
@@ -101,7 +101,7 @@ else{
 			correctcolor:'0.40',
 			clippingpath:'2.40',
 			basicretouch:'0.60',
-			dollar:'19.38'
+			dollar:'19.04'
 		},
 		register:{
 			usermustactivate: false,
@@ -149,7 +149,7 @@ else{
 			correctcolor:'0.40',
 			clippingpath:'2.40',
 			basicretouch:'0.60',
-			dollar:'19.38'
+			dollar:'19.04'
 		},
 		register:{
 			usermustactivate: false,

c75766f6d87104e75cb78a34bf890f4c6eb1e5a1
Johnny <jerh56@gmail.com>
2016-12-10 22:13:06 -0700

    Se quitara funciones innecesarias de paypal que estaban de prueba.

--- routes/paypal.js
+++ routes/paypal.js
@@ -1,123 +1,13 @@
-  var express = require('express');
-  var config = require('../config');
-  var Orders = require('../models/order.js');
-  var OrderPacks = require('../models/orderpacks.js');
-  var router = express.Router();
-  var paypal = require('paypal-rest-sdk');
-  require('../config_paypal');
-
-var paymentId = "PAY-3HK76932GS852015ELBCI4LA";
-var create_payment_json = {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "redirect_urls": {
-        "return_url": config.paypal.return_url,
-        "cancel_url": config.paypal.cancel_url
-    },
-    "transactions": [{
-        "item_list": {
-            "items": [{
-                "name": "item",
-                "sku": "item",
-                "price": "1.00",
-                "currency": "USD",
-                "quantity": 1
-            }]
-        },
-        "amount": {
-            "currency": "USD",
-            "total": "1.00"
-        },
-        "description": "This is the payment description."
-    }]
-};
-
-// var execute_payment_json = {
-//         "payer_id": payer_id,
-//         "transactions": [{
-//             "amount": {
-//                 "currency": "USD",
-//                 "total": "1.00"
-//             }
-//         }]
-//     };
+var express = require('express');
+var config = require('../config');
+var Orders = require('../models/order.js');
+var OrderPacks = require('../models/orderpacks.js');
+var router = express.Router();
+var paypal = require('paypal-rest-sdk');
+require('../config_paypal');
 
 
-
-router.get('/getpayment', function(req, res) {
-     paypal.payment.get(paymentId, function (error, payment) {
-        if (error) {
-            console.log(error);
-           // throw error;
-           res.setHeader('Content-Type', 'application/json');
-            res.send(error);  
-        } else {
-            console.log("Get Payment Response");
-            console.log(JSON.stringify(payment));
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(payment));  
-        }
-
-    });
-});
-
-router.get('/createpayment', function(req, res) {
-  paypal.payment.create(create_payment_json, function (error, payment) {
-      if (error) {
-          console.log(error);
-             // throw error;
-          res.setHeader('Content-Type', 'application/json');
-          res.send(error); 
-      } else {
-      
-          console.log("Create Payment Response");
-          console.log(payment);
-          //res.setHeader('Content-Type', 'application/json');
-          //res.send(payment);  
-          var href;
-          console.log(href);
-          for (var index = 0; index < payment.links.length; index++) {
-          //Redirect user to this endpoint for redirect url
-              if (payment.links[index].rel === 'approval_url') {
-                  console.log(payment.links[index].href);
-                  href = payment.links[index].href;
-              }
-          }
-
-          if (href != null){
-            res.redirect(href);
-          }
-          else
-          {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(payment);  
-          }
-      }
-  });
-});
-
-
-router.get('/executepayment', function(req, res) {
-
-  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-      if (error) {
-         console.log(error);
-             // throw error;
-          res.setHeader('Content-Type', 'application/json');
-          res.send(error); 
-      } else {
-          console.log("Get Payment Response");
-          console.log(JSON.stringify(payment));
-
-          res.setHeader('Content-Type', 'application/json');
-          res.send(JSON.stringify(payment));  
-      }
-  });
-});
-
-router.get('/return', function(req, res){
+router.get('/return', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
   //todo revisar sesion
   console.log(req.body);
   console.log(req.query);
@@ -189,25 +79,18 @@ router.get('/return', function(req, res){
 
 });
 
-router.get('/cancel', function(req, res){
+router.get('/cancel', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
   //todo revisar sesion
   console.log(req.body);
   console.log(req.query);
+  var paymentId = req.query['paymentId'];
+  var payer_id = req.query['PayerID']; 
+  var execute_payment_json = {
+        "payer_id": payer_id
+  };
+  res.redirect( '/error' );
 
 });
-
-
-// function getPayment(paymentid,cb){
-//   paypal.payment.get(paymentid, function (err, payment) {
-//         if (err) {
-//           console.log(err);
-//           cb(1,'Error al consultar el pago: ' + err);
-//         } else {
-//           cb(0,'',payment);   
-//         }
-
-//     });
-// };
 
 module.exports = router;
 

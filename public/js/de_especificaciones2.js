669b9a1db301d875a228248cea44787ccd856285
Johnny <jerh56@gmail.com>
2017-04-18 05:24:38 -0600

    Se cambio DPI por dpi para coincidir con el modelo y se mejoró la forma de mostrar las especificaciones.

--- public/js/de_especificaciones2.js
+++ public/js/de_especificaciones2.js
@@ -23,8 +23,6 @@ $(document).ready(function(){
                             //$('#specnombre').focus();
                     }
                     else{
-                        //setTimeout(window.location='/subirimagen4.html',500);
-                        //alert(data.spec.sizenone);
                         $('#specnombre').prop("value", data.spec.name);
                         $('#specnombre').focus();
                         $('#format').prop("value", data.spec.format);
@@ -32,7 +30,13 @@ $(document).ready(function(){
                         $('#background').prop("value", data.spec.background);
                         $('#colorselect').prop("value", data.spec.backgrndcolor);
 
-
+                        if (data.spec.background === 'negro' ){
+                            $('#colorselect').prop("value", '#000');
+                        }
+                        
+                        if (data.spec.background === 'sin_fondo' ){
+                            $('#colorselect').hide();
+                        }
                         // if (data.spec.sizenone==null){
                         //     $('#chksizenone').prop('checked', false);
                         // }
@@ -40,36 +44,60 @@ $(document).ready(function(){
                         if (data.spec.dpinone !== null){
                             if (data.spec.dpinone === 'none'){
                                 $('#chkDPInone').prop('checked',true);
+                                $('#divDPIOptions').hide();
                             }
                             else{
                                 $('#chkDPInone').prop('checked',false);
+                                $('#divDPINone').hide();
                             }
                         }
                         else{
                             $('#chkDPInone').prop('checked',false);
+                            $('#divDPINone').hide();
                         }
 
 
                         if (data.spec.sizenone !== null){
                             if (data.spec.sizenone === 'none'){
                                 $('#chksizenone').prop('checked',true);
+                                $('#divSizeOptions').hide(); 
+                                $('#divSizeCustomize').hide();
                             }
                             else{
                                 $('#chksizenone').prop('checked',false);
+                                $('#divSizeNone').hide();
                             }
                         }
                         else{
                             $('#chksizenone').prop('checked',false);
+                            $('#divSizeNone').hide();
+                        }
+
+                        if (data.spec.marginnone !== null){
+                            if (data.spec.marginnone === 'none'){
+                                $('#chkmarginone').prop('checked',true);
+                                $('#divMarginOptions').hide(); 
+                            }
+                            else{
+                                $('#chkmarginone').prop('checked',false);
+                                $('#divMarginNone').hide();
+                            }
+                        }
+                        else{
+                            $('#chkmarginone').prop('checked',false);
+                            $('#divMarginNone').hide();
                         }
                         
 
-                            if (data.spec.alignnone !== null){
+                        if (data.spec.alignnone !== null){
                             if (data.spec.alignnone === 'none'){
                                 $('#chkalgnnone').prop('checked',true);
+                                $('#divAlignOptions').hide();    
+
                             }
                             else{
                                 $('#chkalgnnone').prop('checked',false);
-
+                                $('#divAlignNone').hide();
                                 var alignhor = data.spec.alignhor;
                                 $('#'+ alignhor).prop('checked', true);  
                                 var alignver = data.spec.alignver;
@@ -81,6 +109,7 @@ $(document).ready(function(){
                         }
                         else{
                             $('#chkalgnnone').prop('checked',false);
+                            $('#divAlignNone').hide();
                         }
                         
 
@@ -88,7 +117,7 @@ $(document).ready(function(){
                             $('#marginmeasure').prop('value', data.spec.marginmeasure);
                         }
                         
-                        if (data.spec.margintop !== null && data.spec.margintop != undefined){
+                        if (data.spec.margintop !== null && data.spec.margintop !== undefined){
                             $("#specForm input[name='margintop']").prop('value', data.spec.margintop);
                         }
                         if (data.spec.marginbottom !== null && data.spec.marginbottom !== undefined){
@@ -101,43 +130,68 @@ $(document).ready(function(){
                             $("#specForm input[name='marginright']").prop('value', data.spec.marginright);
                         }
 
+
+                        $('#divExtras').children().hide();
                         if (data.spec.naturalshadow !== null && data.spec.naturalshadow !== undefined){  
                             $("#specForm input[name='naturalshadow']").prop( "checked", true );
-                            $("#specForm input[name='dropshadow']").prop( "checked", false );
+                            $("#specForm input[name='naturalshadow']").show();
+                            $("#spNaturalShadow").show();
+                           // $("#specForm input[name='dropshadow']").prop( "checked", false );
                                     
                         }  
                         if (data.spec.dropshadow !== null && data.spec.dropshadow !== undefined){  
-                            $("#specForm input[name='naturalshadow']").prop( "checked", false );
+                           // $("#specForm input[name='naturalshadow']").prop( "checked", false );
                             $("#specForm input[name='dropshadow']").prop( "checked", true );
+                            $("#specForm input[name='dropshadow']").show();
+                            $("#spDropShadow").show();
                         }  
                         if (data.spec.correctcolor !== null && data.spec.correctcolor !== undefined){  
                             $("#specForm input[name='correctcolor']").prop( "checked", true );
+                            $("#specForm input[name='correctcolor']").show();
+                            $("#spCorrectColor").show();
                         }  
 
                         if (data.spec.clippingpath !== null && data.spec.clippingpath !== undefined){  
                             $("#specForm input[name='clippingpath']").prop( "checked", true );
+                            $("#specForm input[name='clippingpath']").show();
+                            $("#spClippingPath").show();
                         }  
 
                         if (data.spec.basicretouch !== null && data.spec.basicretouch !== undefined){  
                             $("#specForm input[name='basicretouch']").prop( "checked", true );
+                            $("#specForm input[name='basicretouch']").show();
+                            $("#spBasicRetouch").show();
                         }  
 
                         if (data.spec.imagesize !== null && data.spec.imagesize !== undefined){
                             // $('input:radio[name=imagesize]').prop('value',data.spec.imagesize);
+                            $('#divSizeOptions').children().hide();
+                            $('#divSizeCustomize').hide();
+                            
                             if (data.spec.imagesize === '1:1') {
                                 $('#radio11').prop('checked', 'true');
+                                $('#radio11').show();
+                                $('#spRadio11').show();
                             }
                             if (data.spec.imagesize === '2:3') {
                                 $('#radio23').prop('checked', 'true');
+                                $('#radio23').show();
+                                $('#spRadio23').show();
                             }
                             if (data.spec.imagesize === '3:2') {
                                 $('#radio32').prop('checked', 'true');
+                                $('#radio32').show();
+                                $('#spRadio32').show();
                             }
                             if (data.spec.imagesize === '4:3') {
                                 $('#radio43').prop('checked', 'true');
+                                $('#radio43').show();
+                                $('#spRadio43').show();
                             }
                             if (data.spec.imagesize === 'customize') {
                                 $('#optcustomsize').prop('checked', 'true');
+                                $('#divSizeCustomize').show();
+                            
                             }
                         }
                         if (data.spec.dpi !== null && data.spec.dpi !== undefined){
@@ -283,9 +337,11 @@ $(document).ready(function(){
         var chkalgnnone = document.getElementById('chkalgnnone').checked;
         $('input:radio[name=alignhor]').prop('disabled', chkalgnnone);
         $('input:radio[name=alignver]').prop('disabled', chkalgnnone);
+        //$('input[type=radio]').hide();
+        //$('#divSize').hide();
     }
 
-        function desactMarginAllNext(){
+    function desactMarginAllNext(){
         var chkmarginone = document.getElementById('chkmarginone').checked;
         $('#marginmeasure').prop('disabled', chkmarginone);
         $('input[name=margintop]').prop('disabled', chkmarginone);
